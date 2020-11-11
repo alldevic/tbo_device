f7dd11dbb05059af3ae33a02e5bcb9c25693d2bb
Niikolay Bely <belyynikolay@gmail.com>
2020-11-11 15:23:31 +0700

    Add base project

---
+++ tbo_device/tbo_device.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    // waiting
+  }
+  Serial.println("Hello world!");  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
