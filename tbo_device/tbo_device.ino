5ce5dc8990b6f6032fdca1657676df457c9f8b75
Niikolay Bely <belyynikolay@gmail.com>
2020-11-14 13:43:56 +0700

    Add sketch.json

--- tbo_device/tbo_device.ino
+++ tbo_device/tbo_device.ino
@@ -1,12 +1,13 @@
 void setup() {
   Serial.begin(9600);
+  
   while (!Serial) {
     // waiting
   }
+
   Serial.println("Hello world!");  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
